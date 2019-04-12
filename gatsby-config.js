9817347fcfa7ff92a9c82dd596f7907a2a89f341
aaronactivate <aaron@activate.co.uk>
2019-04-12 23:32:53 +0100

    120419

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,6 +1,18 @@
 module.exports = {
   siteMetadata: {
-    title: 'Design+Code 3',
-  },
-  plugins: ['gatsby-plugin-react-helmet'],
+	title: 'Design+Code 3',
+	description: 'Complete courses about the best tools and design systems. Prototype and build apps with React and Swift. 60 hours of video content and resource materials. No coding experience required.',
+	keywords: 'react course, react for designers, ios development, sketch app, swift app course, arkit 2, after effects, create sketch plugin',
+  author: '@butler952',
+},
+  plugins: [
+    'gatsby-plugin-react-helmet',
+    {
+      resolve: 'gatsby-source-contentful',
+      options: {
+        spaceId: 't098slj4wgi4',
+        accessToken: '127f14276db8611ab5e69486888027c964a1df096b2f6200652b7316d081b99d'
+      }
+    }
+  ],
 }
--- src/layouts/index.js
+++ src/layouts/index.js
@@ -4,27 +4,60 @@ import Helmet from 'react-helmet'
 
 import Header from '../components/header'
 import './index.css'
+import Footer from '../components/Footer';
 
 const Layout = ({ children, data }) => (
   <div>
     <Helmet
       title={data.site.siteMetadata.title}
       meta={[
-        { name: 'description', content: 'Sample' },
-        { name: 'keywords', content: 'sample, something' },
+        {
+          name: `description`,
+          content: data.site.siteMetadata.description,
+        },
+                {
+          name: `keywords`,
+          content: data.site.siteMetadata.keywords,
+        },
+        {
+          property: `og:title`,
+          content: data.site.siteMetadata.title,
+        },
+        {
+          property: `og:url`,
+          content: `https://www.aaronbutlerdesign.co.uk/`,
+        },
+        {
+          property: `og:description`,
+          content: data.site.siteMetadata.description,
+        },
+        {
+          property: `og:type`,
+          content: `website`,
+        },
+        {
+          name: `twitter:card`,
+          content: `summary`,
+        },
+        {
+          name: `twitter:creator`,
+          content: data.site.siteMetadata.author,
+        },
+        {
+          name: `twitter:title`,
+          content: data.site.siteMetadata.title,
+        },
+        {
+          name: `twitter:description`,
+          content: data.site.siteMetadata.description,
+        },
       ]}
     />
-    <Header siteTitle={data.site.siteMetadata.title} />
-    <div
-      style={{
-        margin: '0 auto',
-        maxWidth: 960,
-        padding: '0px 1.0875rem 1.45rem',
-        paddingTop: 0,
-      }}
-    >
-      {children()}
-    </div>
+    <Header />
+    {children()}
+    <Footer data={data}>
+      Backgrounds made in Cinema 4D, iOS app in Swift, site in React. <a href="mailto:support@designcode.io">Email us</a> to ask anything. © 2018
+    </Footer>
   </div>
 )
 
@@ -39,6 +72,18 @@ export const query = graphql`
     site {
       siteMetadata {
         title
+        description
+        keywords
+        author
+      }
+    }
+    allContentfulLink(sort: { fields: [createdAt], order: ASC }) {
+      edges {
+        node {
+          title
+          url
+          createdAt
+        }
       }
     }
   }
